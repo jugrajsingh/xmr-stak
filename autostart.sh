5ba97f41387d3bd83eba99208f70f25047369159
Jugraj Singh <jugraj.singh@paxcom.net>
2017-12-28 22:23:16 +0530

    AutoStart Updated

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,20 @@
+if test $1 = "cuda" ;
+then
+	sudo apt install nvidia-cuda-toolkit
+fi
 sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
-mkdir build
+if ! [ -e "build" ];
+then
+	mkdir build
+fi
 cd build
-cmake .. -DCUDA_ENABLE=OFF
+CUDA="OFF"
+if test $1 = "cuda";
+then
+	CUDA="ON"
+fi
+
+cmake .. -DCUDA_ENABLE=$CUDA
 make
 
 ./bin/xmr-stak -o pool.supportxmr.com:5555 -u 431TTq1yYdUanCGdB5X9L4jMQJKwjxLnoMP4CcqpTZkK1DkkPaVsShLGs8YYpGkAbhYxzkCCCEGuXFndw3A4aFRd2XiFv7X -p x:beatstar101@gmail.com --use-nicehash --currency monero
