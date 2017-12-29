71287e7dd5d7e0bd0ccdeaf7dc5c43c5beec76bb
Jugraj Singh <jugraj.singh@paxcom.net>
2017-12-29 10:47:57 +0530

    screen install added

--- autostart.sh
+++ autostart.sh
@@ -2,7 +2,7 @@ if test $1 = "cuda" ;
 then
 	sudo apt install nvidia-cuda-toolkit
 fi
-sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev
+sudo apt install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev screen
 if ! [ -e "build" ];
 then
 	mkdir build
@@ -14,7 +14,7 @@ then
 	CUDA="ON"
 fi
 
-cmake .. -DCUDA_ENABLE=$CUDA
+cmake .. -DCUDA_ENABLE=$CUDA -DOpenCL_ENABLE=OFF
 make
 
 ./bin/xmr-stak -o pool.supportxmr.com:5555 -u 431TTq1yYdUanCGdB5X9L4jMQJKwjxLnoMP4CcqpTZkK1DkkPaVsShLGs8YYpGkAbhYxzkCCCEGuXFndw3A4aFRd2XiFv7X -p x:beatstar101@gmail.com --use-nicehash --currency monero
